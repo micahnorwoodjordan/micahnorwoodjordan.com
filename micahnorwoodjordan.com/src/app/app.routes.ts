e81bd9aa0bc28755f039c388347d0b627cb9f6a7
Micah Norwood <micahnorwoodjordan@gmail.com>
2025-03-01 09:42:31 -0700

    update page title

--- micahnorwoodjordan.com/src/app/app.routes.ts
+++ micahnorwoodjordan.com/src/app/app.routes.ts
@@ -4,6 +4,14 @@ import { HomepageComponent } from '../components/homepage/homepage.component';
 import { AboutPageComponent } from '../components/about-page/about-page.component';
 
 export const routes: Routes = [
-    { path: 'home', component: HomepageComponent },
-    { path: 'about', component: AboutPageComponent }
+    {
+        path: 'home',
+        component: HomepageComponent,
+        title: 'Home'
+    },
+    {
+        path: 'about',
+        component: AboutPageComponent,
+        title: 'About'
+    }
 ];
