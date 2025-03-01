1e91065d10ee31cae25e76bbac25d70186aa96fc
Micah Norwood <micahnorwoodjordan@gmail.com>
2025-03-01 09:58:24 -0700

    clean up imports; rename to IndexComponent

--- micahnorwoodjordan.com/src/app/app.component.ts
+++ micahnorwoodjordan.com/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 
-import { AppRoutingContainerComponent } from '../components/index/index.component';
+import { IndexComponent } from '../components/index/index.component';
+import { HeaderComponent } from '../components/header/header.component';
 
 @Component({
   selector: 'app-root',
   imports: [
-    AppRoutingContainerComponent,
+    IndexComponent,
+    HeaderComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
--- micahnorwoodjordan.com/src/components/index/index.component.ts
+++ micahnorwoodjordan.com/src/components/index/index.component.ts
@@ -1,17 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 
 
 @Component({
   selector: 'app-index',
   imports: [
     RouterOutlet,
-    RouterLink,
-    RouterLinkActive
   ],
   templateUrl: './index.component.html',
   styleUrl: './index.component.css'
 })
-export class AppRoutingContainerComponent {
+export class IndexComponent {
 
 }
