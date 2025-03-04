4bb33a7f7b4409c71db4bbb103b523033a925150
Micah Norwood <micahnorwoodjordan@gmail.com>
2025-03-04 01:49:45 -0700

    continue designing about page; tweaked header and footer positioning

--- micahnorwoodjordan.com/src/components/about-page/about-page.component.ts
+++ micahnorwoodjordan.com/src/components/about-page/about-page.component.ts
@@ -4,6 +4,11 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @Component({
   selector: 'app-about-page',
@@ -11,7 +16,11 @@ import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
     RouterOutlet,
     RouterLink,
     RouterLinkActive,
-    FlexLayoutModule
+    FlexLayoutModule,
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatButtonModule
   ],
   templateUrl: './about-page.component.html',
   styleUrl: './about-page.component.css'
