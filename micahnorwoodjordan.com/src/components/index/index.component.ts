c7d3becda3fcff195de17d0bb46022277f470b00
Micah Norwood <micahnorwoodjordan@gmail.com>
2025-03-04 21:01:57 -0700

    finish bottom half of index page; program all buttons

--- micahnorwoodjordan.com/src/components/index/index.component.ts
+++ micahnorwoodjordan.com/src/components/index/index.component.ts
@@ -1,9 +1,6 @@
 import { Component } from '@angular/core';
-
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatButtonModule } from '@angular/material/button';
-
-
 
 
 @Component({
@@ -16,5 +13,10 @@ import { MatButtonModule } from '@angular/material/button';
   styleUrl: './index.component.css'
 })
 export class IndexComponent {
+  myGithubURL: string = 'https://github.com/micahnorwoodjordan';
+  myMusicURL: string = 'https://getmybeats.com';
+  myLinkedinURL: string = 'https://www.linkedin.com/in/micah-norwood777';
+  myAboutPageURL: string = 'http://localhost:4200/about';
 
+  navigateToURL(url: string) { window.location.href = url; }
 }
