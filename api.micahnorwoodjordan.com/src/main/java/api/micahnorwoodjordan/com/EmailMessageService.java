a011c183553ce5f12082ede9d19e4ed57b5f32d5
Micah Norwood <micahnorwoodjordan@gmail.com>
2025-03-05 23:53:45 -0700

    configure emailing functionality

--- api.micahnorwoodjordan.com/src/main/java/api/micahnorwoodjordan/com/EmailMessageService.java
+++ api.micahnorwoodjordan.com/src/main/java/api/micahnorwoodjordan/com/EmailMessageService.java
@@ -1,12 +1,34 @@
 package api.micahnorwoodjordan.com;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailMessageService {
+
+    private static final String MICAH_EMAIL_ADDRESS = "micahnorwoodjordan@gmail.com";
+
+    @Autowired
+    private JavaMailSender mailSender;
+
     @Autowired
     private EmailMessageRepository emailMessageRepository;
+
+    private boolean send(String to, String subject, String body) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        try {
+            message.setTo(to);
+            message.setSubject(subject);
+            message.setText(body);
+            mailSender.send(message);
+            return true;
+        } catch(Exception e) {
+            System.out.println(e);
+        }
+        return false;
+    }
 
     public EmailMessage getEmailMessage(long emailMessageId) {
         return emailMessageRepository.findById(emailMessageId);
@@ -14,7 +36,7 @@ public class EmailMessageService {
 
     public boolean sendEmailMessage(EmailMessage emailMessage) {
         try {
-            // TODO: wire up email-sending logic
+            send(MICAH_EMAIL_ADDRESS, "Test from Java App", "Disregard; this is a test.");
             emailMessageRepository.save(emailMessage);
             return true;
         } catch(Exception e) {
