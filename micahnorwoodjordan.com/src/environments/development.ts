82cb1162b7a69825b64b799832390b8891909b16
Micah Norwood <micahnorwoodjordan@gmail.com>
2025-03-11 19:38:45 -0700

    fetch assets from static site over ssl; include nginx conf

--- micahnorwoodjordan.com/src/environments/development.ts
+++ micahnorwoodjordan.com/src/environments/development.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: false,
     clientUrl: 'http://localhost:4200',
     apiUrl: 'http://localhost:8080',
-    staticSiteUrl: 'http://64.227.111.158'
+    staticSiteUrl: 'https://static.micahnorwoodjordan.com'
   };
--- micahnorwoodjordan.com/src/environments/production.ts
+++ micahnorwoodjordan.com/src/environments/production.ts
@@ -2,5 +2,5 @@ export const environment = {
     production: true,
     clientUrl: 'https://micahnorwoodjordan.com',
     apiUrl: 'https://api.micahnorwoodjordan.com',
-    staticSiteUrl: 'http://64.227.111.158'
+    staticSiteUrl: 'https://static.micahnorwoodjordan.com'
   };
