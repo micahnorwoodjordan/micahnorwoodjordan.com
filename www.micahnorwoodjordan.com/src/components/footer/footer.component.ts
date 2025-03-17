55223f16fba913e425dd8f9ee6758a00ebfa9357
Micah Norwood <micahnorwoodjordan@gmail.com>
2025-03-16 20:25:21 -0700

    conditionally render footer

--- www.micahnorwoodjordan.com/src/components/footer/footer.component.ts
+++ www.micahnorwoodjordan.com/src/components/footer/footer.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { NgIf } from '@angular/common';
+import { ContextService } from '../../app/services/context.service';
 
 @Component({
   selector: 'app-footer',
   imports: [
-    FlexLayoutModule
+    FlexLayoutModule,
+    NgIf
   ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
+  constructor(private contextService: ContextService) {  }
 
+  joshuaVerse: string = `"Have I not commanded you? Be strong and of good courage; do not be afraid, nor be dismayed, for the Lord your God is with you wherever you go. - NKJV"`
+
+  getUserIsOnMobile() { return this.contextService.userIsOnMobile; }
 }
