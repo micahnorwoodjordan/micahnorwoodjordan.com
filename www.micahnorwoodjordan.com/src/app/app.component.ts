733db7e41792917b238e3b0d0e6f24ba834e07d0
Micah Norwood <micahnorwoodjordan@gmail.com>
2025-04-01 23:22:21 -0700

    add element to follow along with user scroll

--- www.micahnorwoodjordan.com/src/app/app.component.ts
+++ www.micahnorwoodjordan.com/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef } from '@angular/core';
+import { NgIf } from '@angular/common';
 
 import { FlexLayoutModule } from '@angular/flex-layout';
 
@@ -6,7 +7,7 @@ import { BlankComponent } from './components/blank/blank.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { CanvasComponent } from './components/canvas/canvas.component';
-
+import { ContextService } from './services/context.service';
 
 @Component({
   selector: 'app-root',
@@ -15,11 +16,43 @@ import { CanvasComponent } from './components/canvas/canvas.component';
     HeaderComponent,
     FooterComponent,
     CanvasComponent,
-    FlexLayoutModule
+    FlexLayoutModule,
+    NgIf
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  constructor(private contextService: ContextService, private mobileNavRef: ElementRef) {  }
+
   title = 'micahnorwoodjordan.com';
+  mobileNav: HTMLElement | null = null;
+  windowHeight = window.innerHeight || document.documentElement.clientHeight;
+
+  setMobileNav(htmlElement: HTMLElement) { this.mobileNav = htmlElement; }
+  
+
+
+  ngAfterViewInit() {
+    if (this.getUserIsOnMobile()) {
+      window.addEventListener('load', () => {
+        if (this.mobileNav !== null) {
+          let newPosition = window.scrollY + (this.windowHeight / 2);
+          this.mobileNav.style.top = `${newPosition}px`;
+        }
+      });
+
+      this.setMobileNav(this.mobileNavRef.nativeElement.querySelector("#mobile-nav") as HTMLElement);
+      window.addEventListener('scroll', () => {
+        if (this.mobileNav !== null) {
+          
+          let newPosition = window.scrollY + (this.windowHeight / 2);
+          console.log(newPosition);
+          console.log(this.windowHeight);
+          this.mobileNav.style.top = `${newPosition}px`;
+        }
+      });
+    }
+  }
+  getUserIsOnMobile() { return this.contextService.userIsOnMobile; }
 }
