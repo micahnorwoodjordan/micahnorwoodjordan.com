add5c9359f8894ff2ea613aa5da96814134729eb
Micah Norwood <micahnorwoodjordan@gmail.com>
2025-04-04 23:19:40 -0700

    configure model, repository, service layer and controller method

--- api.micahnorwoodjordan.com/src/main/java/api/micahnorwoodjordan/com/Controller.java
+++ api.micahnorwoodjordan.com/src/main/java/api/micahnorwoodjordan/com/Controller.java
@@ -1,5 +1,7 @@
 package api.micahnorwoodjordan.com;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +13,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import api.micahnorwoodjordan.com.dataaccess.models.EmailMessage;
+import api.micahnorwoodjordan.com.dataaccess.models.Project;
 import api.micahnorwoodjordan.com.services.EmailMessageService;
+import api.micahnorwoodjordan.com.services.ProjectService;
 
 @RestController
 @CrossOrigin(origins = {"http://localhost:4200", "http://micahnorwoodjordan.com", "https://micahnorwoodjordan.com"})
@@ -19,6 +23,9 @@ public class Controller {
 
     @Autowired
     private EmailMessageService emailMessageService;
+
+    @Autowired
+    private ProjectService projectService;
 
     @GetMapping("/ping")
 	public ResponseEntity index() {
@@ -51,5 +58,10 @@ public class Controller {
     @GetMapping("/notifications/email")
 	public ResponseEntity<EmailMessage> getEmailMessage(@RequestParam(name = "id", required = true) long emailMessageId) {
 		return new ResponseEntity<>(emailMessageService.getEmailMessage(emailMessageId), HttpStatus.OK);
+	}
+
+    @GetMapping("/projects")
+	public ResponseEntity<List<Project>> getProjects() {
+		return new ResponseEntity<>(projectService.getAllProjects(), HttpStatus.OK);
 	}
 }
