a59384497993430febc3d5f3ea2d4d4570285e96
Micah Norwood <micahnorwoodjordan@gmail.com>
2025-04-05 18:32:41 -0700

    call api and use ngfor to display project cards

--- www.micahnorwoodjordan.com/src/app/components/projects-page/projects-page.component.ts
+++ www.micahnorwoodjordan.com/src/app/components/projects-page/projects-page.component.ts
@@ -1,29 +1,42 @@
 import { Component } from '@angular/core';
-import { NgIf } from '@angular/common';
+import { NgIf, NgFor, AsyncPipe } from '@angular/common';
+import { of } from 'rxjs';
+
 import { FlexLayoutModule } from '@angular/flex-layout';
 
+import { environment } from '../../../environments/production';
 import { ProjectCardComponent } from '../project-card/project-card.component';
 import { ContextService } from '../../services/context.service';
+
+import { Project } from '../../interfaces/Project';
+import { ApiService } from '../../services/api.service';
+import { Observable } from 'rxjs';
 
 
 @Component({
   selector: 'app-projects-page',
   imports: [
+    AsyncPipe,
     FlexLayoutModule,
     NgIf,
+    NgFor,
     ProjectCardComponent
   ],
   templateUrl: './projects-page.component.html',
   styleUrl: './projects-page.component.css'
 })
 export class ProjectsPageComponent {
-  constructor(private contextService: ContextService) {  }
+  constructor(private contextService: ContextService, private apiService: ApiService) {
+    this.projects$ = this.apiService.getAllProjects();
+  }
 
   imageURL: string = "";
   bodyText: string = "";
   projectName: string = "";
   projectSubtitle: string = "";
   projectURL: string = "";
+
+  projects$: Observable<Project[]>;
 
   getUserIsOnMobile(): boolean { return this.contextService.userIsOnMobile; }
 }
