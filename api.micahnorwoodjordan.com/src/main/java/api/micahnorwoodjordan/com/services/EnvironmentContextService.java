85cb98a7d00186f85b63aac5f47b0360ee177776
Micah Norwood <micahnorwoodjordan@gmail.com>
2025-04-22 13:49:31 -0700

    always print env name

--- api.micahnorwoodjordan.com/src/main/java/api/micahnorwoodjordan/com/services/EnvironmentContextService.java
+++ api.micahnorwoodjordan.com/src/main/java/api/micahnorwoodjordan/com/services/EnvironmentContextService.java
@@ -35,7 +35,8 @@ public class EnvironmentContextService {
                                 }
                         }
 
-                        System.out.println("Detected active non-production profile: " + env.getActiveProfiles()[0].toUpperCase());
 		}
+
+                System.out.println("Detected active non-production profile: " + env.getActiveProfiles()[0].toUpperCase());
 	}
 }
