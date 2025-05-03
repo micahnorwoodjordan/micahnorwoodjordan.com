e147197f68946246be6cd7d8982114c2a9cc991c
Micah Norwood <micahnorwoodjordan@gmail.com>
2025-05-03 07:57:18 -0700

    forgot to make the call to install from s3

--- static.micahnorwoodjordan.com/app.py
+++ static.micahnorwoodjordan.com/app.py
@@ -1,5 +1,7 @@
 from startup import create_app
 
+from startup.install_artifacts import install_s3_artifacts
+
 from environment.environment import ENV, HOST
 
 
@@ -10,6 +12,7 @@ app = create_app()
 
 
 if __name__ == '__main__':
+    install_s3_artifacts()
     app.run(
         host=HOST,
         debug=True if ENV['DEBUG'] == 'true' else False,  # TODO: this can be handled more gracefully
