41b89982c4ea15e2fb3c48253e08a42dddaa7505
Micah Norwood <micahnorwoodjordan@gmail.com>
2025-05-03 08:11:41 -0700

    write gunicorn logs to stdout and stderr

--- static.micahnorwoodjordan.com/gunicorn.conf.py
+++ static.micahnorwoodjordan.com/gunicorn.conf.py
@@ -1,7 +1,7 @@
 
 bind = "0.0.0.0:443"
 workers = 2
-accesslog = "/var/log/static.micahnorwoodjordan.com/access.log"
-errorlog = "/var/log/static.micahnorwoodjordan.com/error.log"
+accesslog = "-"
+errorlog = "-"
 loglevel = "info"
 timeout = 30
