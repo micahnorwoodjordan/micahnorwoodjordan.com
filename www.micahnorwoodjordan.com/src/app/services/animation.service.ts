8666f737a2b858d8c24815194dec3ade2b4682b4
Micah Norwood <micahnorwoodjordan@gmail.com>
2025-05-10 17:27:46 -0700

    add comments and organization to text decryption effect section

--- www.micahnorwoodjordan.com/src/app/services/animation.service.ts
+++ www.micahnorwoodjordan.com/src/app/services/animation.service.ts
@@ -12,13 +12,11 @@ export class AnimationService {
 
   constructor() { }
 
+  // -----------------------------------------------generic animations----------------------------------------------
   readonly transitionDurationString: string = "1s";
   readonly scrollYPositionCoefficient: number = 1.75;
   readonly easeAnimationString: string = "in(0.7)";
   readonly redrawIntervalMilliseconds: number = 1000;
-
-  // for text decryption visual effect
-  private interval: any;
 
   private elementIsValid(element: any): boolean { return element !== null && element !== undefined; }
   private logNullElementMessage(methodName: string) { console.log(`${methodName}: target element is null`); }
@@ -31,39 +29,52 @@ export class AnimationService {
       this.logNullElementMessage(animationDescription);
     }
   }
-
-  applyDecryptionEffectToMarkupText(targetText: string, callback: (text: string) => void, speed: number = 100): void {
-    let characterIndex = 0;
-
-    this.clear(); // Clear any existing interval
-
-    this.interval = setInterval(() => {
-      if (characterIndex >= targetText.length) {
-        callback(targetText); // Final state
-        this.clear();
-        return;
-      }
-
-      let scramble = '';
-      for (let j = 0; j < targetText.length - characterIndex - 1; j++) {
-        scramble += this.randomChar();
-      }
-
-      const partial = targetText.slice(0, characterIndex + 1) + scramble;
-      callback(partial);
-
-      characterIndex++;
-    }, speed);
-  }
+  // -----------------------------------------------generic animations----------------------------------------------
+  // -----------------------------------------------text decryption effect------------------------------------------
+  private interval: any;
 
   private randomChar(): string {
     return Constants.matrixCharacters.charAt(Math.floor(Math.random() * Constants.matrixCharacters.length));
   }
 
-  clear(): void {
+  private clear(): void {
     if (this.interval) {
       clearInterval(this.interval);
       this.interval = null;
     }
   }
+
+  applyDecryptionEffectToMarkupText(
+    targetText: string,
+    callback: (text: string) => void,
+    speed: number = 100
+  ): void {
+    // make a copy of the "who is micah" string
+    // set every character in the copy to a randomly generated character sequence consisting of matrix characters
+    // every 1/10 seconds by default, replace one character in the copy with one true character from the true "who is micah" string
+    // by calling the callback function, which should update some string attribute of a component with the partially "decrypted" text
+
+    let decryptedCharIdx = 0;
+
+    this.clear(); // Clear any existing interval
+
+    this.interval = setInterval(() => {
+      if (decryptedCharIdx >= targetText.length) {
+        callback(targetText); // Final state
+        this.clear();
+        return;
+      }
+
+      let encryptedText = '';
+      for (let count = 0; count < targetText.length - decryptedCharIdx - 1; count++) {
+        encryptedText += this.randomChar();
+      }
+
+      const partiallyDecryptedText = targetText.slice(0, decryptedCharIdx + 1) + encryptedText;
+      callback(partiallyDecryptedText);
+
+      decryptedCharIdx++;
+    }, speed);
+  }
+  // -----------------------------------------------text decryption effect------------------------------------------
 }
