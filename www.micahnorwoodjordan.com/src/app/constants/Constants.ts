6619622dde33ae89a5b0cb2a7614ea2a3929c9f8
Micah Norwood <micahnorwoodjordan@gmail.com>
2025-05-10 21:29:55 -0700

    apply decryption effect for about page text

--- www.micahnorwoodjordan.com/src/app/constants/Constants.ts
+++ www.micahnorwoodjordan.com/src/app/constants/Constants.ts
@@ -4,5 +4,9 @@ export class Constants {
         public static readonly apiV2HeaderValue: string = "v2";
 
         // animations
-        public static readonly matrixCharacters = 'アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッンABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+        public static readonly matrixCharacters: string = 'アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッンABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+        public static readonly decryptionEffectSpeedMillisSlow: number = 100;
+        public static readonly decryptionEffectSpeedMillisFast: number = 50;
+        public static readonly decryptionEffectSpeedMillisFaster: number = 25;
+        public static readonly decryptionEffectSpeedMillisFastest: number = 12;
 }
