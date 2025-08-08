fbc23a91654492338dfde4e4e041507837a1818e
Micah Norwood <micahnorwoodjordan@gmail.com>
2025-08-08 11:18:41 -0700

    update s3 logic for digitalocean hosting

--- static.micahnorwoodjordan.com/startup/tasks/install_artifacts.py
+++ static.micahnorwoodjordan.com/startup/tasks/install_artifacts.py
@@ -1,8 +1,6 @@
 from aws.s3.client import S3Client
 
-from environment.environment import ENV
-
 
 def install_s3_artifacts():
-    s3_client = S3Client(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'], ENV['REGION_NAME'], ENV['BUCKET'])
-    s3_client.download_all()
+    s3_client = S3Client()
+    s3_client.sync('static')
